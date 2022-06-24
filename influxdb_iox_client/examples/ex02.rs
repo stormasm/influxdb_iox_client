656e9d3e8951e2de35759e3c7aa37f4d8978c2f3
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2022-06-24 09:05:36 -0700

    mw

--- influxdb_iox_client/examples/ex02.rs
+++ influxdb_iox_client/examples/ex02.rs
@@ -1,20 +1,3 @@
-/*
-#[tokio::main]
-async fn main() {
-    use influxdb_iox_client::{connection::Builder, health::Client};
-
-    let connection = Builder::default()
-        .build("http://127.0.0.1:8082")
-        .await
-        .unwrap();
-
-    let mut client = Client::new(connection);
-
-    let x = client.check_storage().await.expect("check_storage failure");
-    println!("{:?}", x);
-}
-*/
-
 #[tokio::main]
 async fn main() {
     use influxdb_iox_client::{
